96920886331dba41231087b87c3cb366cc084355
Rory Collins <rorycollins@me.com>
2023-01-25 22:16:34 +0000

    add more tests to Card component

--- src/components/Card/__test__/Card.test.tsx
+++ src/components/Card/__test__/Card.test.tsx
@@ -3,8 +3,9 @@ import renderer from "react-test-renderer";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { IdeaType } from "../../../utilities/types";
 import { IdeaContext } from "../../../context/IdeaContext";
+import { format } from "date-fns";
 
-describe("Idea component tests", () => {
+describe("Card component tests", () => {
   let idea: IdeaType = {
     id: "1",
     title: "Example idea",
@@ -33,7 +34,76 @@ describe("Idea component tests", () => {
     expect(descriptionInput).toBeInTheDocument();
   });
 
-  it("should submit the form and call the handleSubmission function with the correct data", async () => {
+  it("should display the timestamp with the correct format", () => {
+    const { getByText } = render(
+      <IdeaContext.Provider value={{ dispatch, ideas }}>
+        <Card idea={idea} />
+      </IdeaContext.Provider>
+    );
+    const timestamp = getByText(
+      format(new Date(idea.timestamp), "yyyy-MM-dd - HH:mm:ss")
+    );
+    expect(timestamp).toBeInTheDocument();
+  });
+
+  it("should render the 'Add' button when no idea is passed as a prop", () => {
+    render(<Card />);
+    const addButton = screen.getByText("Add");
+    expect(addButton).toBeInTheDocument();
+  });
+
+  it("should have a 'Delete' button and be able to delete an idea when clicked", async () => {
+    const { getByText } = render(
+      <IdeaContext.Provider value={{ dispatch, ideas }}>
+        <Card idea={idea} />
+      </IdeaContext.Provider>
+    );
+
+    const deleteButton = getByText("Delete");
+    expect(deleteButton).toBeInTheDocument();
+
+    fireEvent.click(deleteButton);
+    await waitFor(() => {
+      expect(dispatch).toHaveBeenCalledWith({
+        type: "DELETE_IDEA",
+        payload: {
+          id: idea.id
+        }
+      });
+    });
+  });
+
+  it("should have a 'Reset' button and be able to reset the form when clicked", async () => {
+    const { getByText } = render(
+      <IdeaContext.Provider value={{ dispatch, ideas }}>
+        <Card />
+      </IdeaContext.Provider>
+    );
+
+    const resetButton = getByText("Reset");
+    expect(resetButton).toBeInTheDocument();
+
+    fireEvent.change(screen.getByPlaceholderText("Title"), {
+      target: { value: "Test Title" }
+    });
+    fireEvent.change(screen.getByPlaceholderText("Description"), {
+      target: { value: "Test Description" }
+    });
+
+    expect(screen.getByPlaceholderText("Title")).toHaveValue("Test Title");
+    expect(screen.getByPlaceholderText("Description")).toHaveValue(
+      "Test Description"
+    );
+
+    fireEvent.click(resetButton);
+
+    await waitFor(() => {
+      expect(screen.getByPlaceholderText("Title")).toHaveValue("");
+      expect(screen.getByPlaceholderText("Description")).toHaveValue("");
+    });
+  });
+
+  it("should submit the form, call handleSubmission function and update card with the correct data", async () => {
     render(
       <IdeaContext.Provider value={{ dispatch, ideas }}>
         <Card idea={idea} />
--- src/components/CardList/__test__/CardList.test.tsx
+++ src/components/CardList/__test__/CardList.test.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { IdeaContext } from "../../../context/IdeaContext";
 import { CardList } from "../CardList";
 import { IdeaType } from "../../../utilities/types";
 
-describe("CardList", () => {
+describe("CardList tests", () => {
   const dispatch = jest.fn();
   const ideas: IdeaType[] = [
     {
